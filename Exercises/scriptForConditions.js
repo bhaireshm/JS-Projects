969744f896eb59dd4a580e23dae802ed3364f2b8
Bhairesh <bhairesh97@gmail.com>
2019-08-19 14:44:35 +0530

    modified script used consitional statements

--- Exercises/scriptForConditions.js
+++ Exercises/scriptForConditions.js
@@ -1,6 +1,66 @@
-var fName = "Bhairesh";
-var civilStat = "Single";
+/*
+var fName = "Karthi";
+var civilStat = "single";
 
 if (civilStat === 'married'){
     console.log(fName + " is married!");
+} else {
+    console.log(fName + " hopefully marry soon.");
 }
+
+var isMarried = true;
+
+if (isMarried){
+    console.log(fName + " is married!");
+} else {
+    console.log(fName + " hopefully marry soon.");
+}
+
+*/
+
+
+/*
+var fName = "John";
+var age = 24;
+
+if (age < 13){
+    console.log(fName+ " is a boy.");
+} else if(age >= 13 && age < 20){
+    console.log(fName+ " is a Teenager.");
+} else if (age >= 20 && age < 30){
+    console.log(fName+ " is a young man.");
+}
+else {
+    console.log(fName+ " is a man.");
+}
+
+*/
+
+// ternary opreator
+// switch 
+var fName = "John";
+var age = 16 ;
+ age >= 18 ? console.log(fName+" drinks beer")
+ : console.log(fName+" drinks beer");
+
+var drink = age >= 18 ? "beer" : "juice" ;
+console.log(fName+ " drinks "+drink);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
