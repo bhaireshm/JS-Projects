9a79415fb7ba0375b267425486171dedff7804f2
Bhairesh <bhairesh97@gmail.com>
2019-08-19 16:40:39 +0530

    code challege completed

---
+++ Code Challenge 1/JS/script.js
@@ -0,0 +1,27 @@
+
+// var height = prompt("Whats ur Height: ");
+// var weight = prompt("Whats ur Weight: ");
+
+// var your_BMI = weight / (height * height);
+
+// var height = prompt("Whats ur Friend's Height: ");
+// var weight = prompt("Whats ur Friend's Weight: ");
+
+// var frnds_BMI = weight / (height * height);
+
+// var BMI = your_BMI > frnds_BMI ;
+
+var weightMark = 65; // in kgs
+var heightMark = 165; // in Inches
+var markBmi = weightMark / (heightMark * heightMark);
+
+var weightJohn = 57; // in kgs
+var heightJohn = 173; // in Inches
+var johnBmi = weightJohn / (heightJohn * heightJohn);
+
+console.log(markBmi, johnBmi);
+
+var BMI = markBmi > johnBmi; 
+
+console.log("Mark\'s BMI is Greater");
+
