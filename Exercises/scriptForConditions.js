965b74b023df3e3e155dc1ab21e4c916638eda1b
Bhairesh <bhairesh97@gmail.com>
2019-08-19 16:48:09 +0530

    switch condition , ternary and equality operator added.

--- Exercises/scriptForConditions.js
+++ Exercises/scriptForConditions.js
@@ -36,8 +36,9 @@ else {
 
 */
 
+
+/*
 // ternary opreator
-// switch 
 var fName = "John";
 var age = 16 ;
  age >= 18 ? console.log(fName+" drinks beer")
@@ -46,7 +47,51 @@ var age = 16 ;
 var drink = age >= 18 ? "beer" : "juice" ;
 console.log(fName+ " drinks "+drink);
 
+// switch case 
+var job = "instructor";
+switch (job) {
+    case "teacher" :
+    case "instructor" : // both teacher and instructor job executes the same below code.
+        console.log(fName+" teaches very very good.");
+        break;
+    case "developer" : 
+        console.log(fName+ " developes code very neatly.");
+        break;
+    case "driver" :
+        console.log(fName+" drives vehicle smoothly.");
+        break;      
+    default : 
+        console.log(fName+" does something very good.");
+}
 
+
+age = 35;
+
+switch (true) {
+    case age < 13 :
+        console.log(fName+ " is a boy.");
+        break;
+    case age >= 13 && age < 20 : 
+        console.log(fName+ " is a Teenager.");
+        break;
+    case age >= 20 && age < 30 :
+        console.log(fName + " is a young man.");
+        break;
+    default :
+        console.log(fName+ " is a man.");       
+}
+*/
+
+// Equality Operator
+// Falsy operator : undefined, 0, null, '';
+// Truthy operator: not falsy operator ; 
+
+var height ;
+if (height){
+    console.log("Var is defined.");
+} else {
+    console.log("Not defined or not assigned.");
+}
 
 
 
