3446555f4433206c188744ea5f1e07a71a3b700a
Bhairesh <bhairesh97@gmail.com>
2019-08-22 16:41:52 +0530

    Function Contructions & Object Creations script

---
+++ Advanced-JS/scriptObjectCreation.js
@@ -0,0 +1,32 @@
+var personProto = {
+    calcAge : function(){
+        console.log(2019 - this.YOB);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = "John";
+john.YOB = 1990;
+john.job = "designer";
+
+var jane = Object.create(personProto,
+    {
+        name : { value : "Jane"},
+        YOB : { value : 1998 },
+        job : { value : "Dancer"}
+    }
+    );
+
+    var naanu = Object.create(
+        {
+            name : { value : "Naanu"},
+            YOB : { value : 1980}
+        }
+    );
+
+
+
+
+console.log(naanu);
+console.log(jane);
+console.log(john);
