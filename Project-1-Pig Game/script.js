268bf64c9773fe43dfc49643ad88000ea176cf1f
Bhairesh <bhairesh97@gmail.com>
2019-08-22 11:57:57 +0530

    PIG Game script modified for Code challenge 6.3

--- Project-1-Pig Game/script.js
+++ Project-1-Pig Game/script.js
@@ -4,33 +4,44 @@ var score, roundScore, activePlayer, gamePlaying, winScore;
 
 init();
 
+function randomNumber(){
+    return (Math.floor(Math.random() * 6) + 1);
+}
+
 var prevsDice;
 document.querySelector('.btn-roll').addEventListener('click', function(){
 
     if(gamePlaying){
     // random number
-    var dice =  Math.floor(Math.random() * 6) + 1;
-        
+    var dice1 =  randomNumber();
+    var dice2 =  randomNumber();
+    var diceTot = dice1 + dice2;  
     // display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    var diceDOM1 = (document.querySelector('#dice-1')) ;
+    diceDOM1.style.display = 'block';
+    diceDOM1.src = 'dice-' + dice1 + '.png';
+
+    var diceDOM2 = (document.querySelector('#dice-2')) ;
+    diceDOM2.style.display = 'block';
+    diceDOM2.src = 'dice-' + dice2 + '.png';
+
 
     // update the roundscore if the score is not equal to zero
        // console.log(dice,prevsDice);
-        if(dice === 6 && prevsDice === 6){
+        if(dice1 === 6 && dice2 === 6){
            // player looses score
-            score[activePlayer] = 0 ;            
+            score[activePlayer] = 0 ;   
+            document.querySelector('#current-'+ activePlayer).textContent = score[activePlayer];         
 
-        }else if(dice !== 1){ //  SAME AS (dice > 1) // doesnt do type coerition
+        }else if(dice1 !== 1 && dice2 !== 1){ //  SAME AS (dice > 1) // doesnt do type coerition
          // add score
-            roundScore += dice;
+            roundScore += diceTot;
             document.querySelector('#current-'+ activePlayer).textContent = roundScore;
-            prevsDice = dice;
         } else {
             // next player
             nextPlayer();
         }
+      //  prevsDice = dice;
     }
 }) // end of roll btn click
 
@@ -51,7 +62,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
     if(score[activePlayer] >= winScore  ){
         document.querySelector('#name-'+activePlayer).textContent = 'Winner!!'
-        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('#dice-1').style.display = 'none';
+        document.querySelector('#dice-2').style.display = 'none';
 
         document.querySelector('.player-'+activePlayer+"-panel").classList.remove('active');
         document.querySelector('.player-'+activePlayer+"-panel").classList.add('winner');
@@ -83,7 +95,8 @@ function nextPlayer(){
             document.querySelector('.player-0-panel').classList.toggle('active');
             document.querySelector('.player-1-panel').classList.toggle('active');
 
-            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('#dice-1').style.display = 'none';
+            document.querySelector('#dice-2').style.display = 'none';
 
             // document.querySelector('.player-0-panel').classList.remove('active');
             // document.querySelector('.player-1-panel').classList.add('active');
@@ -97,7 +110,8 @@ function init(){
     activePlayer = 0;
     gamePlaying = true;
     
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('#dice-1').style.display = 'none';
+    document.querySelector('#dice-2').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0'; // setting value to the elements which is in the html
     document.getElementById('score-1').textContent = '0'; // setting value to the elements which is in the html
@@ -105,8 +119,8 @@ function init(){
     document.getElementById('current-0').textContent = '0'; // setting value to the elements which is in the html
     document.getElementById('current-1').textContent = '0'; // setting value to the elements which is in the html
     
-    // document.querySelector('#name-0').textContent = prompt("Player 1 Name : "); //'Player 1' ;
-    // document.querySelector('#name-1').textContent = prompt("Player 2 Name : ");//'Player 2' ;
+    document.querySelector('#name-0').textContent = prompt("Player 1 Name : "); //'Player 1' ;
+    document.querySelector('#name-1').textContent = prompt("Player 2 Name : ");//'Player 2' ;
 
     if((document.querySelector('#name-0').textContent) == '' && (document.querySelector('#name-1').textContent) == ''){
         document.querySelector('#name-0').textContent = "Player 1";
@@ -123,9 +137,23 @@ function init(){
 }
 
 function changeWinScore(){
-    winScore = prompt("Enter New Winning score Value");
-}
+    var newWinscore = prompt("Enter New Winning score Value");
+  //  console.log(newWinscore);
 
+    if((document.querySelector('#name-0').textContent) == 'Winner!!' || 
+        (document.querySelector('#name-1').textContent) == 'Winner!!' ||
+            (document.querySelector('#name-0').textContent) == '' && (document.querySelector('#name-1').textContent) == ''){
+                init();
+    } else {
+        if(document.getElementById('score-0').textContent == newWinscore ||
+                (document.getElementById('score-1').textContent == newWinscore)){
+                alert("Cannot enter the score value..!!");
+                changeWinScore();
+        } else {
+            winScore = newWinscore;
+        }
+    } 
+}
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;  #current is id name 
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
