325416ddc6c680f2c5dcf5d26bf7730087bc2448
Bhairesh <bhairesh97@gmail.com>
2019-08-26 22:49:22 +0530

    Adding Budget Calculator WebApp (UI is done)

---
+++ Budgety/script.js
@@ -0,0 +1,32 @@
+
+
+var descriptionDOM = document.querySelector('.add-description');
+var amountValueDOM = document.querySelector('.add-value');
+
+var descrValue = descriptionDOM.textContent;
+var amtValue = amountValueDOM.textContent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
